afd42e9f4aeed6af333de43753cc3f007547c90d
MiyaiKasane <white.snow.ice.girl@gmail.com>
2024-01-14 15:48:55 +0900

    Initial commit

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\Model; //エロクワントを使用するための宣言
 use Illuminate\Support\Facades\DB; //クエリビルダを使用する宣言
 use Illuminate\Http\Request; //アプリケーション内でリクエストを処理するためのクラス
-use App\Models\Companies; //Companyモデルを使うための宣言
-use App\Models\Products; //Productモデルを使うための宣言
+use App\Models\Company\companies; //Companyモデルを使うための宣言
+use App\Models\Product\products; //Productモデルを使うための宣言
 use App\Models;
 
 class CompaniesController extends Controller
 {
     public function preView(){
-        $model = new Products();
+        $model = new Product();
         $products = $model->products();
         dd($products);
     }
