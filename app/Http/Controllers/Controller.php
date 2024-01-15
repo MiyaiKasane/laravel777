f07f7bf2de531dc7df276ce088850a657bf952c8
MiyaiKasane <white.snow.ice.girl@gmail.com>
2024-01-16 08:12:25 +0900

    Initial commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Company; //Companyモデルを使うための宣言
-use App\Models\Product; //Productモデルを使うための宣言
+use App\Models\Company\companies; //Companyモデルを使うための宣言
+use App\Models\Product\products; //Productモデルを使うための宣言
 
 class Controller extends BaseController
 {
