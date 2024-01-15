cf6cbcc68c62eba3eb040f98921aad143bea1959
MiyaiKasane <white.snow.ice.girl@gmail.com>
2024-01-16 08:17:25 +0900

    initial commit

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\Model; //エロクワントを使用するための宣言
 use Illuminate\Support\Facades\DB; //クエリビルダを使用する宣言
 use Illuminate\Http\Request; //アプリケーション内でリクエストを処理するためのクラス
-use App\Models\Company\companies; //Companyモデルを使うための宣言
-use App\Models\Product\products; //Productモデルを使うための宣言
+use App\Models\companies; //Companyモデルを使うための宣言
+use App\Models\products; //Productモデルを使うための宣言
 use App\Models;
 
 class CompaniesController extends Controller
 {
+    public function preView(){
+        $model = new Product();
+        $products = $model->products();
+        dd($products);
+    }
 
     public function showList()
     {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,15 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Company\companies; //Companyモデルを使うための宣言
-use App\Models\Product\products; //Productモデルを使うための宣言
+use App\Models\companies; //Companyモデルを使うための宣言
+use App\Models\products; //Productモデルを使うための宣言
 
 class Controller extends BaseController
 {
-    public function preView(){
-        $model = new Product();
-        $products = $model->products();
-        dd($products);
-    }
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
