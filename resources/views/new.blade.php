fc632f9ee5602a33ac38a2d265ec68e66e4ecd7d
MiyaiKasane <white.snow.ice.girl@gmail.com>
2025-04-02 16:42:36 +0900

    Initial commit

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -36,11 +36,11 @@
                 <div>
                         <div class="formSell">
                             <label for="productName" class="thLabel">商品名<span class="Astalisk">*</span></label>
-                            <input id="productName" type="text" name="productName" class="input" value="{{ old('productName') }}">
+                            <input id="productName" type="text" name="productName" class="input">
                         </div>
                         <div class="formSell">
                             <label for="Choice" class="thLabel">メーカー名<span class="Astalisk">*</span></label>
-                            <select id="Choice" name="Choice" class="input" value="{{ old('Choice') }}">
+                            <select id="Choice" name="Choice" class="input">
                                 @foreach($companies as $company)
                                 <option value="{{ $company->id }}">{{ $company->company_name }}</option>
                                 @endforeach
@@ -48,19 +48,19 @@
                         </div>
                         <div class="formSell">
                             <label for="Price" class="thLabel">価格<span class="Astalisk">*</span></label>
-                            <input id="Price" type="text" name="Price" class="input" value="{{ old('Price') }}">
+                            <input id="Price" type="text" name="Price" class="input">
                         </div>
                         <div class="formSell">
                             <label for="Stock" class="thLabel">在庫数<span class="Astalisk">*</span></label>
-                            <input id="Stock" type="text" name="Stock" class="input" value="{{ old('Stock') }}">
+                            <input id="Stock" type="text" name="Stock" class="input">
                         </div>
                         <div class="formSell">
                             <label for="Comment" class="thLabel">コメント</label>
-                            <textarea rows="3" cols="30" id="Comment" name="Comment" class="input" value="{{ old('Comment') }}"></textarea>
+                            <textarea rows="3" cols="30" id="Comment" name="Comment" class="input"></textarea>
                         </div>
                         <div class="formSell">
                             <label for="Image" class="thLabel">商品画像</label>
-                            <input id="Image" type="file" name="Image" class="input" value="{{ old('Image') }}">
+                            <input id="Image" type="file" name="Image" class="input">
                         </div>
                             <button type="submit" class="newsubmit">新規登録</button>
                             <button type="button" class="back" onclick="location.href='{{ route('list') }}'">戻る</button>
